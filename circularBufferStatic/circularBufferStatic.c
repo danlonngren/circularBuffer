b5b64be29b6e8610421e2bb235f23ea2feb5abd5
DanL <dlonngren@gmail.com>
2025-04-10 20:41:14 +0100

    Update name to include static for clarity

--- circularBufferStatic/circularBufferStatic.c
+++ circularBufferStatic/circularBufferStatic.c
@@ -1,4 +1,4 @@
-#include "circularBuffer.h"
+#include "circularBufferStatic.h"
 
 #include <stdio.h>
 #include <string.h>
--- circularBufferStatic/circularBufferStatic.h
+++ circularBufferStatic/circularBufferStatic.h
@@ -1,5 +1,5 @@
-#ifndef CIRCULARBUFFER_H
-#define CIRCULARBUFFER_H
+#ifndef CIRCULARBUFFERSTATIC_H
+#define CIRCULARBUFFERSTATIC_H
 
 #include <stdint.h>
 #include <stdbool.h>
@@ -12,13 +12,13 @@ CBUFFER_STATUS_EMPTY            = -3,
 CBUFFER_STATUS_FAILED           = -4,
 }cBufferStatus_t;
 
-#define LIST_INIT() {   \
-    .capacity = 0,      \
-    .dataSize = 0,      \
-    .put_index = 0,     \
-    .get_index = 0,     \
-    .counter = 0,       \
-    .dataBuffer = NULL }
+#define LIST_INIT(cap, size) { \
+    .capacity = (cap),         \
+    .dataSize = (size),        \
+    .put_index = (0),          \
+    .get_index = (0),          \
+    .counter = (0),            \
+    .dataBuffer = (NULL) }
 
 typedef struct {
     uint32_t capacity;
@@ -36,20 +36,14 @@ bool circularBufferIsFull(circularBufferList_t *pList);
 
 uint32_t circularBufferCapacity(circularBufferList_t *pList);
 
-
 cBufferStatus_t circularBufferCreate(circularBufferList_t *config,  void *dataBuffer, uint32_t dataSize, uint32_t capacity);
 
 cBufferStatus_t circularBufferReset(circularBufferList_t *config);
 
-
 cBufferStatus_t circularBufferGet(circularBufferList_t *config, void *pDataOut);
 
-/* Will override the data if full */
 cBufferStatus_t circularBufferPut(circularBufferList_t *config, void *pData);
 
-/*  */
 cBufferStatus_t circularBufferPutSafe(circularBufferList_t *config, void *pData);
-
-
 
 #endif
